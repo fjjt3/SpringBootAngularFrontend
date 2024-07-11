241ba72ebf809de9341fdea2171c41f39b4e7ccc
Francisco Jiménez <fjjt3@yahoo.com>
2024-07-11 13:34:29 +0200

    Add:agregar producto ts

---
+++ src/app/editar-producto/editar-producto.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-editar-producto',
+  templateUrl: './editar-producto.component.html',
+  styleUrls: ['./editar-producto.component.css']
+})
+export class EditarProductoComponent {
+  producto: Producto = new Producto();
+  id: number;
+
+  constructor(private productoServicio: ProductoService,
+    private ruta: ActivatedRoute,
+    private enroutador: Router){}
+
+  ngOnInit(){
+    // get id from the URL
+    this.id = this.ruta.snapshot.params['id'];
+    // retrieve the product by Id 
+    this.productoServicio.obtenerProductoPorId(this.id).subscribe(
+      {
+        next: (datos) => this.producto = datos
+        ,
+        error: (errores: any) => console.log(errores)
+
+      }
+    )
+  }
+}
